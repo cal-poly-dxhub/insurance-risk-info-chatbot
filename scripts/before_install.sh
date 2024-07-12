82f9dec8cf6fcd84fe946786dd6fa5acc4690861
Swayam Chidrawar <schidraw@calpoly.edu>
2024-07-12 15:17:47 -0700

    fix: deployment

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+set -e
+
+# Update the system
 sudo yum update -y
+
+# Install Python 3 and pip if not already installed
 sudo yum install -y python3 python3-pip
+
+# Install venv module
+sudo yum install -y python3-venv
